7eb50dbc1df628e9da7cce768bfa11ca624ce69c
Ismail Hossain <ridoy.hossain@mail.utoronto.ca>
2017-05-10 21:06:39 -0400

    Updated Pokemon model

--- Pokedex/Pokemon.swift
+++ Pokedex/Pokemon.swift
@@ -10,26 +10,54 @@ import Foundation
 
 class Pokemon {
     
-    private var _pokemonName: String!
+    private var _name: String!
     private var _pokedexID: Int!
-    private var description: String!
-    private var pokemonType: String!
-    private var attack: String!
-    private var defense: String!
-    private var height: String!
-    private var weight: String!
-    private var nextEvolution: String!
+    private var _description: String!
+    private var _type: String!
+    private var _attack: String!
+    private var _defense: String!
+    private var _height: String!
+    private var _weight: String!
+    private var _nextEvolution: String!
     
     var name: String {
-        return _pokemonName
+        return _name
     }
     
     var pokedexID: Int {
         return _pokedexID
     }
     
+    var description: String {
+        return _description
+    }
+    
+    var type: String {
+        return _type
+    }
+    
+    var attack: String {
+        return _attack
+    }
+    
+    var defense: String {
+        return _defense
+    }
+    
+    var height: String {
+        return _height
+    }
+    
+    var weight: String {
+        return _weight
+    }
+    
+    var nextEvolution: String {
+        return _nextEvolution
+    }
+    
     init(name: String, pokedexID: Int) {
-        self._pokemonName = name.capitalized
+        self._name = name.capitalized
         self._pokedexID = pokedexID
     }
     
